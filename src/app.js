b453f0152a16df596988560dfd28d8c058c4f3a5
dukelw <thevi16102004@gmail.com>
2024-01-13 18:08:42 +0700

    Bắt 2 lỗi sai path và lỗi bên trong hệ thống

--- src/app.js
+++ src/app.js
@@ -24,4 +24,21 @@ require("./databases/connect-mongodb");
 route(app);
 
 // Handling errors
+app.use((req, res, next) => {
+  res.status(404);
+  res.json({
+    status: 404,
+    message: "Not found your URL request",
+  });
+});
+
+app.use((error, req, res, next) => {
+  const statusCode = error.status || 500;
+  return res.status(statusCode).json({
+    status: "error",
+    code: statusCode,
+    message: error.message || "Internal server error",
+  });
+});
+
 module.exports = app;
