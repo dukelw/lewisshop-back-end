716e315bf2e004d2fdbdf6334a9f87a7ab3f9215
dukelw <thevi16102004@gmail.com>
2024-02-21 00:12:30 +0700

    Chỉnh sửa redirect về trang cảm ơn Momo

--- src/services/payment.js
+++ src/services/payment.js
@@ -10,10 +10,8 @@ class PaymentService {
       const accessKey = "F8BBA842ECF85";
       const secretKey = "K951B6PE1waDMi640xX08PD3vg6EkVlz";
       const orderInfo = "pay with MoMo";
-      const redirectUrl =
-        "https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b";
-      const ipnUrl =
-        "https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b";
+      const redirectUrl = "http://localhost:1610/thanks";
+      const ipnUrl = "http://localhost:1610/thanks";
       const orderId = uuid.v4();
       const requestId = uuid.v4();
       const requestType = "captureWallet";
